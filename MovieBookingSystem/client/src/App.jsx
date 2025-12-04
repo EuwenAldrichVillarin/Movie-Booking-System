e6c2fcdbeca3d91b09c5b0c579b89691e9ead6d5
HengWeida <gioneri1022@gmail.com>
2025-12-04 10:00:50 +0800

    booking, my tickets, url no access without login

--- MovieBookingSystem/client/src/App.jsx
+++ MovieBookingSystem/client/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import { BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+
 import Landing from "./Pages/Landing.jsx";
 import Layout from "./Components/Layout.jsx";
 import Login from './Pages/Login.jsx';
@@ -14,31 +13,39 @@ import FAQ from './Pages/FAQ.jsx';
 import PrivacyPolicy from './Pages/PrivacyPolicy.jsx';
 import Checkout from './Pages/Booking/Checkout.jsx';
 import MyTickets from './Pages/Booking/MyTickets.jsx';
+
+import ProtectedRoute from "./Components/ProtectedRoute.jsx";
+
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-     <Router>
+    <Router>
       <Routes>
-           <Route path="/" element={<Login />} />
-           <Route element={<Layout />}>
-        <Route path="/Home" element={<Landing />} />
-         <Route path="/my-tickets" element={<MyTickets />} />
-        <Route path="/movies" element={<BookingMovie />} />
-        <Route path="/movies-checkout" element={<Checkout />} />
-         <Route path="/features" element={<Features />} />
-         <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          
-          {/* Support Pages */}
-          <Route path="/help-center" element={<HelpCenter />} />
-          <Route path="/faq" element={<FAQ />} />
-          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-          
-          {/* Catch all - redirect to home */}
-          <Route path="*" element={<Navigate to="/home" replace />} />
+        {/* Public route */}
+        <Route path="/" element={<Login />} />
+
+        {/* Protected routes */}
+        <Route element={<ProtectedRoute />}>
+          <Route element={<Layout />}>
+            <Route path="/Home" element={<Landing />} />
+            <Route path="/my-tickets" element={<MyTickets />} />
+            <Route path="/movies" element={<BookingMovie />} />
+            <Route path="/movies-checkout" element={<Checkout />} />
+            <Route path="/features" element={<Features />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+
+            {/* Support Pages (protected too, since you listed them) */}
+            <Route path="/help-center" element={<HelpCenter />} />
+            <Route path="/faq" element={<FAQ />} />
+            <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+
+            {/* Catch all - redirect to Home */}
+            <Route path="*" element={<Navigate to="/Home" replace />} />
+          </Route>
         </Route>
       </Routes>
     </Router>
